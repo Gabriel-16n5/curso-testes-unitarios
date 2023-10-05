3a7112851655f66a2617a24946c0391ec95a6f46
GabrielIGNS <gabrielgranjeia@alunos.utfpr.edu.br>
2023-10-05 03:19:08 -0300

    aula 5 e exec

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# curso-testes-unitarios
-exercícios
+Aprendendo a fazer tests unitários
+Criar a pasta unit dentro da pasta de tests
+Dentro da pasta de unit criar um arquivo para testar as coisas da service, o nome fica, nomeDaService.test.ts
---
+++ aula05/tests/unit/pacientes.test.ts
@@ -0,0 +1,19 @@
+import { faker } from "@faker-js/faker";
+import { generateProtocolForPacient } from "../../src/protocols-generator";
+import { v4 as uuidv4 } from 'uuid';
+
+jest.mock('uuid', () => {
+  return {
+    v4: () => {return 'protocol'}
+  }
+})
+
+describe("protocol test", () => {
+  it("mocked protocol test", async () => {
+    const name = faker.person.firstName();
+    const lastName = faker.person.lastName();
+    const sapo = generateProtocolForPacient(name, lastName, false)
+    console.log(sapo)
+    expect(sapo.protocol).toBe('protocol');
+  });
+});
